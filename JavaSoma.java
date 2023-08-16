b3089bd16a39b717975aeb0e0a99ddfc74ee07c4
GabriellMartiim <141282516+GabriellMartiim@users.noreply.github.com>
2023-08-15 21:36:29 -0300

    Add files via upload

---
+++ JavaSoma.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javasoma;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gabriel_oliveira29
+ */
+public class JavaSoma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner entrada = new Scanner (System.in);
+        System.out.println("informe sua primeria nota:");
+        int n1 = entrada.nextInt();
+        System.out.println("informe sua segunda nota:");
+        int n2 = entrada.nextInt();
+        System.out.println("informe sua terceira nota:");
+        int n3 = entrada.nextInt();
+                
+                
+                
+        
+      
+              
+    }
+    
+}
