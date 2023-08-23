2e6b7a622753f5dbdee277e598f5e5d116319088
Gabriel Martim de Oliveira <141282516+GabriellMartiim@users.noreply.github.com>
2023-08-23 18:59:50 -0300

    Add files via upload

---
+++ JavaApplication11.java
@@ -0,0 +1,22 @@
+
+package javaapplication11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gabriel_oliveira29
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
+
+
+  
