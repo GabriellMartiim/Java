d3c2daf7fc37d8fe40776dd4647a8f5dc87e8646
Gabriel Martim de Oliveira <141282516+GabriellMartiim@users.noreply.github.com>
2023-09-18 19:12:41 -0300

    For - Loops
    exercícios em For - Loops
    SENAI

---
+++ For1.java
@@ -0,0 +1,17 @@
+
+package for1;
+
+import java.util.Scanner;
+
+
+public class For1 {
+
+    
+    public static void main(String[] args) {
+       Scanner n = new Scanner (System.in);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
